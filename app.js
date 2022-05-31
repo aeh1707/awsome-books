a61c6c92faa5579afbd18e8c3fcb201a30b6726a
Henry <dokinruss@gmail.com>
2022-05-31 13:13:08 +0000

    Create Script to add object to local storage and display it once window load

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
-outputField = document.querySelector('#outputField');
 
-const books = [
-    {
-        title: 'Book1',
-        author: 'Author1'
-    },
-    {
-        title: 'Book2',
-        author: 'Author2'
-    }
-];
+window.addEventListener('load', ( )=>{
+    bookList = JSON.parse(localStorage.getItem('bookList') ) || [];
+    const inputform = document.getElementById('bookForm');
 
-function Add(title, author) {
-    return `<article>
-                <h2 class="title">${title}</h2>
-                <h3 class="author">${author}</h3>
-                <button type="submit" class="remove-btn">Remove</button>
-                <hr>`;
+    inputform.addEventListener("submit", e =>{
+        e.preventDefault();
+
+        const book = {
+            bookTitle: e.target.elements.bookTitle.value,
+            bookAuthor: e.target.elements.bookAuthor.value,
+
+        }
+
+        bookList.push(book);
+        localStorage.setItem('bookList', JSON.stringify(bookList));
+        e.target.reset();
+
+        displayBooks();
+    });
+
+    displayBooks();
+})
+
+function displayBooks(){
+    const outputFiled = document.getElementById('outputField');
+    outputFiled.innerHTML = '';
+
+    bookList.forEach(book => {
+        const outputField = document.getElementById("outputField");
+        const bookListDiv = document.createElement('div')
+        const bookName = document.createElement('h2');
+        const bookAuthor = document.createElement('h3');
+        const removeBtn = document.createElement('button');
+        const bookcontWrapper = document.createElement('div');
+        removeBtn.innerText = "Remove";
+        removeBtn.classList.add("delete");
+
+        bookName.innerHTML = `${book.bookTitle}`;
+        bookAuthor.innerHTML = `${book.bookAuthor}`;
+
+        bookcontWrapper.append
+        outputField.append(bookListDiv);
+        bookListDiv.append(bookName, bookAuthor,removeBtn)
+
+        removeBtn.addEventListener('click', e => {
+            bookList = bookList.filter(t => t != book);
+            localStorage.setItem('bookList', JSON.stringify(bookList));
+            displayBooks();
+        })
+    });
+  
+
 }
